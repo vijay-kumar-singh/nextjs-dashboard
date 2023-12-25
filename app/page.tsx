5cd7ba3b62b8f5a5d7509f3627a54461d9cd1ba7
Vijay Singh <vijaysingh.nl@gmail.com>
2023-12-25 19:39:53 +0100

    change static to dynamic render

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to VJ World.</strong> This is the dashboard application developed using{' '}
+            <strong>Welcome to VJWorld.</strong> This is the dashboard application developed using{' '}
             <a href="https://nextjs.org/" className="text-blue-500">
               Next.js
             </a>
