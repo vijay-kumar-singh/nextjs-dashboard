5b2f00a8191be4848ea455ea4366d925dcd8a5c5
Vijay Singh <vijaysingh.nl@gmail.com>
2024-12-24 23:49:25 +0100

    updated style

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Vijay Singh</p>
+      <GlobeAltIcon className="h-8 w-8 rotate-[15deg]" />
+      <p className="text-[25px]">Vijay Singh</p>
     </div>
   );
 }
--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="mb-6 bg-gray-800 px-6 py-4 text-white shadow-md">
       <div className="container mx-auto text-center">
         <p>
-          Built with <span className="text-red-500">React 19</span> and{' '}
-          <span className="text-blue-500">Next.js 15+</span>
+          Built with <span className="text-[#61DAFB]">React 19</span> and{' '}
+          <span className="text-[#0070F3]">Next.js 15+</span>
         </p>
       </div>
     </footer>
